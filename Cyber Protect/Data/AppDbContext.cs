9883f23de3533742b6bbe62c50d735029951cb2a
KamvaMacanda-St10496666 <st10496666@vcconnect.edu.za>
2026-06-03 15:16:27 +0200

    Reapply "Creating models  and scafolled items Add project files."
    This reverts commit a8c471cb906a6e5447de369fd2f5cd054e85b100.

---
+++ Cyber Protect/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Cyber_Protect.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cyber_Protect.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        
+        public DbSet<Incident> Incidents { get; set; }
+
+        public DbSet<Threat> Threats { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        
+    }
+}
---
+++ Cyber Protect/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cyber_Protect.Models
+{
+    public class Employee
+    { 
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Required, StringLength (20)]
+        public string ? FullName { get; set; }
+
+        [Required , EmailAddress]
+         public string ? Email { get; set; } 
+    
+         
+    
+
+    }
+}
