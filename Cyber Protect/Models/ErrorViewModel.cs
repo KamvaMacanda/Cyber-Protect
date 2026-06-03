a8c471cb906a6e5447de369fd2f5cd054e85b100
KamvaMacanda-St10496666 <st10496666@vcconnect.edu.za>
2026-06-03 15:15:10 +0200

    Revert "Creating models  and scafolled items Add project files."
    This reverts commit 890078995dd3e32b8599f11f7d783b64e393035a.

--- Cyber Protect/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cyber_Protect.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
